bbac20c4e31a1395bea6ebead92e3795722b337d
Ivan Tyshchenko <tihon@ciat.net.ua>
2018-09-07 10:59:00 +0300

    register views

--- exchanger_api/urls.py
+++ exchanger_api/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
 
+from .views import MerchantViewSet, OrderViewSet
+
 
 router = routers.DefaultRouter()
+router.register('merchants', MerchantViewSet)
+router.register('orders', OrderViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
